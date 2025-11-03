8512c8d62fe617c299589ad2fd3a1b0822429420
PabloViera22 <p4blo.viera@gmail.com>
2025-11-03 00:04:13 -0300

    tabla con nombres cambiados. Faltan todos los datos

--- funciones/cargar_datos.R
+++ funciones/cargar_datos.R
@@ -1,18 +1,9 @@
-indicadores<-c("GC.DOD.TOTL.GD.ZS", "NY.GDP.MKTP.KD.ZG", "GC.TAX.TOTL.GD.ZS", "FP.CPI.TOTL.ZG")
+indicadores<-c("Deuda_del_Gob"="GC.DOD.TOTL.GD.ZS", 
+               "Tasa_Crecimiento_PBI"="NY.GDP.MKTP.KD.ZG", 
+               "Recaudacion"="GC.TAX.TOTL.GD.ZS", 
+               "IPC"="FP.CPI.TOTL.ZG")
 datos<- WDI(country = "AR",indicator = indicadores,
             start = 1960, 
             end = 2023)
-datos%>%rename(deuda_del_estado=GC.DOD.TOTL.GD.ZS,
-               PBI_crecimiento_porc=NY.GDP.MKTP.KD.ZG,
-               recaudacion_impuesto=GC.TAX.TOTL.GD.ZS,
-               )
-diccionario_wdi <- WDIsearch()
+datos
 
-# 2. Definir el código críptico de la columna que quieres entender
-codigo_a_traducir <- "FP.CPI.TOTL.ZG" 
-
-# 3. Filtrar el diccionario para obtener la descripción
-significado_columna <- diccionario_wdi %>%
-  filter(indicator == codigo_a_traducir) %>%
-  select(indicator, name)
-significado_columna         
