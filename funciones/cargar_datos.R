ec403b7ebc18c5443247efa268ee2e18c17fe50b
PabloViera22 <p4blo.viera@gmail.com>
2025-11-21 20:49:51 -0300

    agregue solo un comando para convertir dataframe a csv

--- funciones/cargar_datos.R
+++ funciones/cargar_datos.R
@@ -1,18 +1,13 @@
 source(here::here("config", "parametros.R"))
 
-# Librerias
-library(tidyverse)
-library(WDI)
-
 indicadores <- c("deuda_gob" = "GC.DOD.TOTL.GD.ZS", 
-               "crecimiento_pbi" = "NY.GDP.MKTP.KD.ZG", 
-               "recaudacion" = "GC.TAX.TOTL.GD.ZS", 
-               "ipc" = "FP.CPI.TOTL.ZG",
-               "pbi_constante" = "NY.GDP.MKTP.KN",
-               "pbi_corriente" = "NY.GDP.MKTP.CD")
+                 "crecimiento_pbi" = "NY.GDP.MKTP.KD.ZG", 
+                 "recaudacion" = "GC.TAX.TOTL.GD.ZS", 
+                 "ipc" = "FP.CPI.TOTL.ZG",
+                 "pbi_constante" = "NY.GDP.MKTP.KN",
+                 "pbi_corriente" = "NY.GDP.MKTP.CD")
 
 # Lista de Países por continente y nivel economico
-
 africa_bajos  <- c("NER","ETH","COD")
 africa_medios <- c("KEN","GHA","ZAF")
 africa_altos  <- c("MUS")
@@ -27,33 +22,28 @@ europa_medios <- c("MDA","ALB","SRB","ROU")
 europa_altos  <- c("DEU","FRA","ESP","ITA","SWE","DNK")
 oceania_medios <- c("FJI")
 oceania_altos  <- c("AUS","NZL")
-
+# lista de paises por continente
 africa <- c(africa_bajos, africa_medios, africa_altos)
 latam <- c(latam_bajos, latam_medios, latam_altos)
 asia <- c(asia_bajos, asia_medios, asia_altos)
 europa <- c(europa_bajos, europa_medios, europa_altos)
 oceania <- c(oceania_medios, oceania_altos)
-
+# lista total de paises
 paises <- c(africa, latam, asia, europa, oceania)
 
 datos <- WDI(country = paises, indicator = indicadores,
             start = 2018, 
             end = 2024)
 datos
+# Exportar los datos a RAW y hacerlos csv
+ruta_completa<- file.path(dir_data_raw, "csv_paises")
 
-#==============================================================================#
-# 
-#==============================================================================#
-
-
-# El banco mundial carece de informacion sobre la deuda publica la recaudacion e inflación.
-
-countries <- wb_countries()
-codigos_paises <- countries[, c("country", "iso2c", "iso3c", "region")]
-head(codigos_paises)
-paises <- c(chile="CHL", colombia="COL", argentina="ARG", brasil="BRA")
-
-options(scipen = 999) # Desactiva la notación científica para los PBI
+write.csv(
+  x = datos,          # Tu data.frame a exportar
+  file = ruta_completa,      # La ruta completa del archivo
+  row.names = FALSE,         # Evita incluir los números de fila como una columna
+  fileEncoding = "UTF-8"     # Codificación para manejar acentos y caracteres especiales
+)
 
 # PBI expresado a miles de millones
 
@@ -61,8 +51,7 @@ datos$pbi_constante <- datos$pbi_constante / 1e9
 datos$pbi_corriente <- datos$pbi_corriente / 1e9
 
 # Redondeo de todas las variables
-
-datos <- datos %>%
+datos_dos_digitos <- datos %>%
   mutate(across(c(deuda_gob, crecimiento_pbi, recaudacion, ipc, pbi_constante, pbi_corriente), 
                 round, digits = 2))
 
