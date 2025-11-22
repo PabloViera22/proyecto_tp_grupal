24742f3f5ed0934468537889d0e1ff5996646e6a
PabloViera22 <p4blo.viera@gmail.com>
2025-11-22 01:21:26 -0300

    agregue todos los paises en el csv

--- data/processed/datos_para_grafico.R
+++ data/processed/datos_para_grafico.R
@@ -1,7 +1,9 @@
-install.packages("readr")
-library(readr)
-
 datos_graficos<-read_csv(ruta_completa)
 datos_graficos
+
+# Los transformamos 
+datos_milmillones<-datos_graficos %>% mutate(pbi_constante_en_millones=pbi_constante/ 1e9)
+datos_2024<-datos_milmillones%>% filter(year==2024)
+
 
 
