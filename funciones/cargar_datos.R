2570279213d1443101fe3e448654be785c17d912
PabloViera22 <p4blo.viera@gmail.com>
2025-11-22 01:21:00 -0300

    agrege a todos los paises en el data.frame del banco mundial

--- funciones/cargar_datos.R
+++ funciones/cargar_datos.R
@@ -4,28 +4,21 @@ indicadores <- c("deuda_gob" = "GC.DOD.TOTL.GD.ZS",
                  "crecimiento_pbi" = "NY.GDP.MKTP.KD.ZG", 
                  "recaudacion" = "GC.TAX.TOTL.GD.ZS", 
                  "ipc" = "FP.CPI.TOTL.ZG",
-                 "pbi_constante" = "NY.GDP.MKTP.KN",
-                 "pbi_corriente" = "NY.GDP.MKTP.CD")
+                 "pbi_constante" = "NY.GDP.MKTP.KD")
 
 # Lista de Países por continente
-
 africa <- c("NER", "ETH", "COD", "KEN", "GHA", "ZAF", "MUS")
-
 america <- c("HTI", "BOL", "HND", "PER", "MEX", "CHL", "URY", "USA", "CAN")
-
 asia <- c("AFG", "NPL", "BGD", "IND", "VNM", "PHL", "CHN", "KOR", "JPN", "SGP")
-
 europa <- c("UKR", "MDA", "ALB", "SRB", "ROU", "DEU", "FRA", "ESP", "ITA", "SWE", "DNK")
-
 oceania <- c("FJI", "AUS", "NZL")
-
 
 # lista total de paises
 
 paises <- c(africa, america, asia, europa, oceania)
 
-datos <- WDI(country = paises, indicator = indicadores,
-            start = 2018, 
+datos <- WDI(country = "all", indicator = indicadores,
+            start = 2015, 
             end = 2024)
 datos
 
