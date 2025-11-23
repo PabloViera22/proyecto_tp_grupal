caeddddd945769c943e4f3c7dba320685084a246
PabloViera22 <p4blo.viera@gmail.com>
2025-11-23 16:31:32 -0300

    CAMBIO DE MOMBRE

---
+++ cargar_datos2.R
@@ -0,0 +1,28 @@
+source(here::here("config", "parametros.R"))
+
+
+indicadores3 <- c(
+  "deuda_gob" ="GC.DOD.TOTL.GD.ZS",   # DEBT (Central government debt)
+  "crecimiento_pbi" ="NY.GDP.MKTP.KD.ZG",   # GDP_GROWTH (GDP growth (annual %))
+  "pbi_p_c" ="NY.GDP.PCAP.KD",      # GDP_P_C (GDP per capita - Constant US$)
+  "apertura" ="NE.TRD.GNFS.ZS",      # OPENNESS (Trade (% of GDP))
+  "formacion_bruta_capital" ="NE.GDI.FTOT.ZS",      # LTOTAL (Inversión - Gross capital formation)
+  "consumo_gobierno" ="NE.CON.GOVT.ZS",      # GOV_EXPEND (Government consumption)
+  "interes_real" = "FR.INR.RINR",         # Interest rate (Real interest rate)
+  "inflacion" = "NY.GDP.DEFL.KD.ZG",   # Inflation (GDP deflator (annual %))
+  "poblacion" = "SP.POP.GROW"          # Population growth
+)
+
+datos3 <- WDI(country = "all", indicator = indicadores3,
+             start = 1990, 
+             end = 2024)
+datos3
+
+
+
+
+
+
+
+
+
