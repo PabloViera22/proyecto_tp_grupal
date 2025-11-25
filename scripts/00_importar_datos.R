2b825f68039ff1afba4e8c6065eba4ec9e00acf4
PabloViera22 <p4blo.viera@gmail.com>
2025-11-25 11:17:43 -0300

    subido el script de importar datos

--- scripts/00_importar_datos.R
+++ scripts/00_importar_datos.R
@@ -1,16 +1,35 @@
 source(here::here("config", "parametros.R"))
-
+# Importar los datos del banco mundial
 datos_crudos <- WDI(country = "all", indicator = indicadores,
               start = 1990, 
               end = 2024)
-write.csv(
-  x = datos_crudos,          # Tu data.frame a exportar
-  file = file.path(dir_data_raw, "csv_datos3"),      # La ruta completa del archivo
-  row.names = FALSE,         # Evita incluir los números de fila como una columna
-  fileEncoding = "UTF-8"     # Codificación para manejar acentos y caracteres especiales
-)
+# Exportar los datos a la carpeta raw
+exportar_data(data = datos_crudos,nombre = "datos_importados_wdi", carpeta = "raw", format = "csv")
 
-exportar_datos(datos_crudos, datos_crudos,carpeta_destino = "raw")
+# Importar metadatos del banco mundial (continente y nivel de ingreso)
+meta <- WDI_data$country
+meta_continente<-meta%>%dplyr::select("iso3c","region", "income")
+# Exportar los datos
+exportar_data(data = meta_continente,nombre = "datos_meta_wdi", carpeta = "raw", format = "csv")
+#==============================================================================#
+# Juntar a través de un join ambas columnas
+#==============================================================================#
+datos_unidos <- datos_crudos %>%
+  left_join(meta_continente, by = "iso3c")
+# Exportar a carpeta de procesado
+exportar_data(data = datos_unidos,nombre = "datos_wdi_con_meta", carpeta = "processed", format = "csv")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
