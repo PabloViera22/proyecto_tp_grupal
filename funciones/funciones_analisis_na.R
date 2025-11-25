3b3fd88fb327d4770f66824df4a269284226e852
PabloViera22 <p4blo.viera@gmail.com>
2025-11-25 19:44:25 -0300

    mas funciones de analisis

--- funciones/funciones_analisis_na.R
+++ funciones/funciones_analisis_na.R
@@ -1,9 +1,99 @@
+analizar_na<-function(tabla, grupo){
+  if (!grupo %in% c("income", "region")){stop("El formato debe ser 'income' o 'region'") }
+  if (!is.data.frame(tabla)) {stop("El objeto 'tabla' debe ser un data frame")}
+  conteo_na_por_columna<-tabla%>%group_by(.data[[grupo]])%>%miss_var_summary()
+  print(conteo_na_por_columna)
+}
+  
+analizar_na_grafico<-function(tabla,grupo){
+  if (!grupo %in% c("Upper middle income", "High income","Lower middle income", "Low income"))
+    {stop("El formato debe ser Upper middle income, High income,Lower middle income, Low income") }
+  if (!is.data.frame(tabla)) {stop("El objeto 'tabla' debe ser un data frame")}
+  
+  var_grupo<-paste("Proporción de faltantes:",grupo)
+  patron_grupo<-paste("Patrón de combinación:", grupo)
+  
+  variables_key <- c("deuda_gob", "interes_real", "formacion_bruta_capital", "consumo_gobierno","apertura")
+  filtro<-tabla%>%filter(income==grupo)  
+  aggr(filtro[, variables_key],
+       col = c('steelblue', 'red'), 
+       numbers = TRUE,
+       sortVars = TRUE,
+       labels = variables_key,
+       cex.axis = 0.8,
+       gap = 3,
+       ylab = c(var_grupo,patron_grupo))
+  }
+# 2. Mostrar solo el nombre de la variable y el conteo de NA
+conteo_na_por_columna2 %>%
+  select(variable, n_miss) %>%
+  print(n = Inf) # 'print(n = Inf)' para ver todas las filas si hay muchas columnas
+nrow(datos3)
 
+#VARIABLE DE OCURRENCIA
+# Matriz de patrones usando VIM
+variables_key <- c("deuda_gob", "interes_real", "formacion_bruta_capital", "consumo_gobierno","apertura")
 
+aggr(datos3[, variables_key],
+     col = c('steelblue', 'red'), 
+     numbers = TRUE,
+     sortVars = TRUE,
+     labels = variables_key,
+     cex.axis = 0.8,
+     gap = 3,
+     ylab = c("Proporción de faltantes", "Patrón de combinación"))
 
+#==============================================================================#
+# TEST DE LITTLE
+#==============================================================================#
+# Preparar datos para el test
+test_de_little<- function(datos){
+datos_para_test <- datos %>%
+  dplyr::select(where(is.numeric)) %>%
+  dplyr::select(where(~any(is.na(.))))
+# Realizar test de Little
+test_mcar <- naniar::mcar_test(datos_para_test)
+return(test_mcar)
+# Crear dataframe con resultados
+resultados_mcar <- data.frame(
+  Estadistico = round(test_mcar$statistic, 2),
+  `Grados de libertad` = test_mcar$df,
+  `P-value` = format(test_mcar$p.value, scientific = TRUE),
+  Conclusion = ifelse(test_mcar$p.value < 0.05, 
+                      "Rechazamos H0: Datos NO son MCAR",
+                      "No rechazamos H0: Datos podrían ser MCAR"))
+resultados_mcar %>%
+  kable(caption = "Resultados del Test de Little para MCAR") %>%
+  kable_styling(bootstrap_options = "striped", full_width = FALSE) %>%
+  column_spec(4, bold = TRUE, 
+              color = ifelse(test_mcar$p.value < 0.05, "red", "green"))
+# RESULTADO ES QUE NO SON DATOS PERDIDOS COMPLETAMNTE AL AZAR
+}
 
+#==============================================================================#
+# IMPUTACION MULTIPLE
+#==============================================================================#
+vactor<-columna("consumo_gobierno", "apertura", "formacion_bruta_capital",
+                "interes_real")
+# Seleccionar variables para imputación
+imputacion_multiple<-function(datos, vector_columna){
+vars_mice <- datos %>%
+  dplyr::select(vactor_columna)
 
+# Configurar y ejecutar MICE
+mice_imp <- mice(vars_mice,
+                 m = 5,            # 5 imputaciones
+                 method = 'pmm',   # Predictive mean matching
+                 seed = 2025,
+                 printFlag = FALSE)
 
+# Ver métodos utilizados
+print(mice_imp$method)
+
+# aca estan los datos imputados
+datos_imp_mice <- complete(mice_imp, 1)
+return(datos_imp_mice)
+}
 
 
 
--- scripts/00_importar_datos.R
+++ scripts/00_importar_datos.R
@@ -15,37 +15,24 @@ exportar_data(data = meta_continente,nombre = "datos_meta_wdi", carpeta = "raw",
 #==============================================================================#
 # Juntar a través de un join ambas columnas
 #==============================================================================#
-datos_unidos <- datos_crudos %>%
+datos_join <- datos_crudos %>%
   left_join(meta_continente, by = "iso3c")
 # Exportar a carpeta de procesado
-exportar_data(data = datos_unidos,nombre = "datos_wdi_con_meta", carpeta = "processed", format = "csv")
+exportar_data(data = datos_join,nombre = "datos_wdi_con_meta", carpeta = "processed", format = "csv")
 
 
-#========================#
+#==============================================================================#
 # IMPORTACION DE DATOS  
-#========================#
-
+#==============================================================================#
 # Datos Banco Mundial
-
 datos_wdi <- read.csv("https://raw.githubusercontent.com/PabloViera22/proyecto_tp_grupal/refs/heads/main/data/processed/datos_wdi_con_meta.csv?token=GHSAT0AAAAAADPYWERTJ6POIHZZUA2IKVEY2JGEJCA")
-
 # Datos de deuda y deficit (Datos.macro)
-
 deuda_deficit <- read.csv2("https://raw.githubusercontent.com/PabloViera22/proyecto_tp_grupal/refs/heads/main/data/processed/deuda_deficit_iso3.csv?token=GHSAT0AAAAAADPYWERT6VROQVRF3LIQKBNA2JGEH2Q")
-
 # Join entre las dos tablas
-
 datos_unidos <- datos_wdi %>% 
-  left_join(
-    deuda_deficit,
-    by = c("iso3c", "year" = "fecha", "country" = "paises")
-  ) %>% 
-  rename(anio = year) %>% 
-  rename(pais = country) %>% 
-  filter(anio %in% c(2017,2020,2023)) %>% 
-  select(-c(
-    iso2c, 
-    deuda_gob))
+  left_join(deuda_deficit,y = c("iso3c", "year" = "fecha", "country" = "paises")
+  ) %>% rename(anio = year) %>% rename(pais = country) %>% 
+  filter(anio %in% c(2017,2020,2023)) %>% dplyr::select(-c(iso2c, deuda_gob))
   
 
 
