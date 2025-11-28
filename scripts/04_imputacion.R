3fb09df5761775556235b6857507f3c0bca929da
PabloViera22 <p4blo.viera@gmail.com>
2025-11-28 16:42:13 -0300

    regresion, inicio de anova

--- scripts/04_imputacion.R
+++ scripts/04_imputacion.R
@@ -1,19 +1,10 @@
 # Importamos los datos del anterior script
 tabla_imputar<-read_csv("D:/Proyecto_Git_TP_Grupal/proyecto_tp_grupal/data//processed/tabla_para_imputacion.csv")
 
+columnas_todas<-names(tabla_imputar)
 col_a_imputar<- c("formacion_bruta_capital", "apertura", "pbi_p_c", "crecimiento_pbi", "consumo_gobierno", "inflacion")
-tabla_imputada<-imputacion_multiple(datos = tabla_imputar, vector_columna = col_a_imputar)
+tabla_imputada<-imputacion_multiple(datos = tabla_imputar, vector_columna = columnas_todas)
 nrow(tabla_imputada)
-
-
-tabla_completa_limpia <- tabla_imputar %>% dplyr::select(-all_of(col_a_imputar))%>%
-  left_join(tabla_imputada, by = c("id", "year"), suffix = c("_orig", "_imp"))
-
-
-
-
-
-
 
 
 exportar_data(data = tabla_imputada, nombre = "tabla_limpia", carpeta = "clean")
--- scripts/09_analisis_de_regresion.R
+++ scripts/09_analisis_de_regresion.R
@@ -1,9 +1,52 @@
-
 # IMPORTAMOS DATOS
 tabla_regresion<-read.csv("D:/Proyecto_Git_TP_Grupal/proyecto_tp_grupal/data//clean/tabla_limpia.csv")
 # HIPOTESIS: DEUDA TIENE REALCION EN FORMA DE U INVERTIDA. EL COEFICIENTE DE DEUDA>0 Y EL COEF. DEUDA^2<0.
 
 
+#==============================================================================#
+# MODELO REGRESION GENERAL CORROBARCION DE HIPÓTESIS
+#==============================================================================#
+modelo_regresion_general <- lm( crecimiento_pbi~ deuda_pbi + deuda_cuadrada+ deficit_pbi + apertura + formacion_bruta_capital + 
+                          consumo_gobierno + inflacion, data = tabla_regresion)
+summary(modelo_regresion_general)
+# A PRIMERA VISTA VEMOS QUE NO PODEMOS DETERMINAR EL SIGNO DE DEUDA Y DEUDA CUADRADA
+
+#==============================================================================#
+# REGRESION PARA INGRESO ALTO
+#==============================================================================#
+ingreso_alto<-tabla_regresion%>%dplyr::filter(income=="High income")
+modelo_regresion_alto <- lm( crecimiento_pbi~ deuda_pbi + deuda_cuadrada+ deficit_pbi + apertura + formacion_bruta_capital + 
+                                  consumo_gobierno + inflacion, data = ingreso_alto)
+summary(modelo_regresion_alto)
+#==============================================================================#
+# REGRESION PARA INGRESO MEDIO
+#==============================================================================#
+ingreso_medio<-tabla_regresion%>%dplyr::filter(income=="Upper middle income")
+modelo_regresion_medio <- lm( crecimiento_pbi~ deuda_pbi + deuda_cuadrada+ deficit_pbi + apertura + formacion_bruta_capital + 
+                               consumo_gobierno + inflacion, data = ingreso_medio)
+summary(modelo_regresion_medio)
+
+#==============================================================================#
+# REGRESION PARA INGRESO MEDIO BAJO
+#==============================================================================#
+ingreso_medio_bajo<-tabla_regresion%>%dplyr::filter(income=="Lower middle income")
+modelo_regresion_medio_bajo <- lm( crecimiento_pbi~ deuda_pbi + deuda_cuadrada+ deficit_pbi + apertura + formacion_bruta_capital + 
+                                consumo_gobierno + inflacion, data = ingreso_medio_bajo)
+summary(modelo_regresion_medio_bajo)
+
+#==============================================================================#
+# REGRESION PARA INGRESO BAJO
+#==============================================================================#
+ingreso_bajo<-tabla_regresion%>%dplyr::filter(income=="Low income")
+modelo_regresion_bajo <- lm( crecimiento_pbi~ deuda_pbi + deuda_cuadrada+ deficit_pbi + apertura + formacion_bruta_capital + 
+                                     consumo_gobierno + inflacion, data = ingreso_bajo)
+summary(modelo_regresion_bajo)
+
+# CONCLUSIÓN 
+# los datos no son concluyentes pero no podemos rechazar la hipotesis de que un 
+#aumento de la deuda publica aumente el crecimiento en el corto plazo y lo reduzca
+#en el largo. Esta hipótesis es significatva para el caso en los paises con ingresos
+#medios
 
 
 
@@ -11,21 +54,4 @@ tabla_regresion<-read.csv("D:/Proyecto_Git_TP_Grupal/proyecto_tp_grupal/data//cl
 
 
 
-vector_x<-c("High income", "Low income", "Lower middle income", "Upper middle income")
-for (x in vector_x) {
-  nuevo<-tabla_regresion%>% filter(income==x)
-  #para crecimiento y deuda
-  modelo_regresion <- lm( crecimiento_pbi~ deuda_gob + apertura + formacion_bruta_capital + 
-                            consumo_gobierno + inflacion, data = tabla_regresion)
-  
-  #para recaudacion y deuda
-  modelo_regresion_2 <- lm(crecimiento_pbi~ deuda_gob + deuda2 + apertura + formacion_bruta_capital + 
-                             consumo_gobierno + inflacion, data = tabla_regresion)
-  # los print
-  print(x)
-  print("#================#crecimietno y deuda #================#")
-  print(summary(modelo_regresion))
-  print("#================#agrego deuda^2#================#")
-  print(summary(modelo_regresion_2))
-  
-}
+
