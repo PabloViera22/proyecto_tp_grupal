7d1c5a6b1eb65beb69822e90b0e99d6efa0ff35e
geroosilva <silvageronimofelipe2002@gmail.com>
2025-11-28 02:56:49 -0300

    creacion de estadisticas desciptivas

--- scripts/05_estadisticas_descriptivas.R
+++ scripts/05_estadisticas_descriptivas.R
@@ -6,29 +6,16 @@ library(ggrepel) # Agregar esta libreria a los nuevos parametros
 
 # IMPORTANTE!!
 # Agregar tabla con el nuevo link cuando el repositorio sea publico (borrar comentario cuando se haga)
-tabla_imputar <- read.csv("https://raw.githubusercontent.com/PabloViera22/proyecto_tp_grupal/refs/heads/main/data/processed/tabla_para_imputacion.csv?token=GHSAT0AAAAAADPYWERTDILAZ5KNJANN25G42JI5MCQ") 
+tabla_imputar <- read.csv("https://raw.githubusercontent.com/PabloViera22/proyecto_tp_grupal/refs/heads/main/data/processed/tabla_para_imputacion.csv?token=GHSAT0AAAAAADPYWERTFRGRCBLFFQ5CM7HK2JJE2BQ") 
 
-#======================================================================
-#                   AÑO 2017: El mundo pre-pandemia                    #                   
-#======================================================================
+#====================================
+#      TABLA DE ANALISIS            #
+#====================================
 
-anio_2017 <- tabla_imputar %>% 
-  filter(year == 2017) %>% 
-  mutate(income = factor(income, levels = c("High income", 
-                                          "Upper middle income", 
-                                          "Lower middle income", 
-                                          "Low income")))
+# ANALISIS GENERAL
 
-#==========================================
-# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
-#==========================================
-
-# Ordeno segun ingreso
-
-# Analizaremos segun ingreso de cada pais
-
-anio_2017 %>%
-  group_by(income) %>%
+analisis_estadistico <- tabla_imputar %>% 
+  group_by(income, year) %>% 
   summarise(
     cantida_paises = n_distinct(country),
     mediana = median(deuda_pbi),
@@ -38,322 +25,172 @@ anio_2017 %>%
     minimo = min(deuda_pbi),
     maximo = max(deuda_pbi)
   ) %>% 
-  arrange(income)
-
-#==================================
-# ANALISIS DEL BOXPLOT y OUTLIERS #
-#==================================
-
-# Outliers dentro de cada grupo
-
-paises_outliers_2017 <- anio_2017 %>% 
-  group_by(income) %>% 
-  mutate(
-    Q1 = quantile(deuda_pbi, 0.25),
-    Q3 = quantile(deuda_pbi, 0.75),
-    IQR = IQR(deuda_pbi),
-    techo = Q3 + 1.5*IQR,
-    piso = Q1 - 1.5*IQR
-  ) %>% 
-  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
-  select(country, income, pbi_p_c, deuda_pbi) %>% 
-  arrange(desc(deuda_pbi))
-  
-print("Para el año 2017, estos paises fueron outliers dentro de sus grupos: ")
-print(paises_outliers_2017)
-
-# Grafico del boxplot 
-
-boxplot_2017 <- ggplot(anio_2017, aes(x = income, y = deuda_pbi, fill = income)) +
-  geom_boxplot(alpha = 0.8) + 
-  
-  # Escala de colores manuales
-    
-  scale_fill_manual(values = c(
-    "High income"         = "#2E8B57",  
-    "Upper middle income" = "#9ACD32",  
-    "Lower middle income" = "#FFA500",  
-    "Low income"          = "#CD5C5C"   
-  )) +
-  
-  # Titulos y etiquetas
-  
-  labs(
-    title = "Distribución de la Deuda Pública para el año 2017",
-    subtitle = "Ordenado segun ingresos de los paises",
-    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
-    x = "",
-    y = "Deuda como % del PBI"
-  ) +
-  
-  # Temas del grafico
-  
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text.x = element_text(size = 10, face = "bold"),
-    plot.caption = element_text(hjust = 0)
-  ) +
-      
-  # Etiqueta outliers
-  
-  geom_text_repel(data = paises_outliers_2017,   
-                  aes(label = country),     
-                  size = 4,                 
-                  box.padding = 0.4
-                  )
-
-print(boxplot_2017)
-
-#======================================================================
-#                   AÑO 2020: El mundo en pandemia                    #                   
-#======================================================================
-  
-anio_2020 <- tabla_imputar %>% 
-  filter(year == 2020) %>% 
+  ungroup() %>% 
   mutate(income = factor(income, levels = c("High income", 
                                             "Upper middle income", 
                                             "Lower middle income", 
                                             "Low income")))
+# ANALISIS CONTEO DE PAISES
 
-#==========================================
-# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
-#==========================================
-
-# Ordeno segun ingreso
-
-# Analizaremos segun ingreso de cada pais
-
-anio_2020 %>%
-  group_by(income) %>%
+datos_conteo <- tabla_imputar %>%
+  mutate(income = factor(income, levels = c("High income", 
+                                            "Upper middle income", 
+                                            "Lower middle income", 
+                                            "Low income"))) %>%
+  group_by(income, year) %>% 
   summarise(
-    cantida_paises = n_distinct(country),
-    mediana = median(deuda_pbi),
-    media = mean(deuda_pbi),
-    desvio = sd(deuda_pbi),
-    iqr = IQR(deuda_pbi),
-    minimo = min(deuda_pbi),
-    maximo = max(deuda_pbi)
+    cantidad_paises = n_distinct(country)
   ) %>% 
-  arrange(income)
+  ungroup()
 
-#==================================
-# ANALISIS DEL BOXPLOT y OUTLIERS #
-#==================================
+totales_por_anio <- datos_conteo %>%
+  group_by(year) %>%
+  summarise(total = sum(cantidad_paises))
 
-# Outliers dentro de cada grupo
+# ANALISIS DE MEDIA y MEDIANA
 
-paises_outliers_2020 <- anio_2020 %>% 
-  group_by(income) %>% 
-  mutate(
-    Q1 = quantile(deuda_pbi, 0.25),
-    Q3 = quantile(deuda_pbi, 0.75),
-    IQR = IQR(deuda_pbi),
-    techo = Q3 + 1.5*IQR,
-    piso = Q1 - 1.5*IQR
-  ) %>% 
-  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
-  select(country, income, pbi_p_c, deuda_pbi) %>% 
-  arrange(desc(deuda_pbi))
-
-print("Para el año 2020, estos paises fueron outliers dentro de sus grupos: ")
-print(paises_outliers_2020)
-
-# Grafico del boxplot 
-
-boxplot_2020 <- ggplot(anio_2020, aes(x = income, y = deuda_pbi, fill = income)) +
-  geom_boxplot(alpha = 0.8) + 
-  
-  # Escala de colores manuales
-  
-  scale_fill_manual(values = c(
-    "High income"         = "#2E8B57",  
-    "Upper middle income" = "#9ACD32",  
-    "Lower middle income" = "#FFA500",  
-    "Low income"          = "#CD5C5C"   
-  )) +
-  
-  # Titulos y etiquetas
-  
-  labs(
-    title = "Distribución de la Deuda Pública para el año 2020",
-    subtitle = "Ordenado segun ingresos de los paises",
-    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
-    x = "",
-    y = "Deuda como % del PBI"
-  ) +
-  
-  # Temas del grafico
-  
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text.x = element_text(size = 10, face = "bold"),
-    plot.caption = element_text(hjust = 0)
-  ) +
-  
-  # Etiqueta outliers
-  
-  geom_text_repel(data = paises_outliers_2020,   
-                  aes(label = country),     
-                  size = 4,                 
-                  box.padding = 0.4
-  )
-
-print(boxplot_2020)
-
-#======================================================================
-#                   AÑO 2023: El mundo post-pandemia                  #                   
-#======================================================================
-
-anio_2023 <- tabla_imputar %>% 
-  filter(year == 2023) %>% 
-  mutate(income = factor(income, levels = c("High income", 
-                                            "Upper middle income", 
-                                            "Lower middle income", 
-                                            "Low income")))
-
-#==========================================
-# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
-#==========================================
-
-# Ordeno segun ingreso
-
-# Analizaremos segun ingreso de cada pais
-
-anio_2023 %>%
-  group_by(income) %>%
+metricas_por_anio <- tabla_imputar %>%
+  group_by(year) %>%
   summarise(
-    cantida_paises = n_distinct(country),
-    mediana = median(deuda_pbi),
-    media = mean(deuda_pbi),
-    desvio = sd(deuda_pbi),
-    iqr = IQR(deuda_pbi),
-    minimo = min(deuda_pbi),
-    maximo = max(deuda_pbi)
-  ) %>% 
-  arrange(income)
+    media   = mean(deuda_pbi, na.rm = TRUE),
+    mediana = median(deuda_pbi, na.rm = TRUE)
+  )
 
-#==================================
-# ANALISIS DEL BOXPLOT y OUTLIERS #
-#==================================
+#====================================
+#             GRAFICOS              #
+#====================================
 
-# Outliers dentro de cada grupo
+# GRAFICO CONTEO de PAISES
 
-paises_outliers_2023 <- anio_2023 %>% 
-  group_by(income) %>% 
-  mutate(
-    Q1 = quantile(deuda_pbi, 0.25),
-    Q3 = quantile(deuda_pbi, 0.75),
-    IQR = IQR(deuda_pbi),
-    techo = Q3 + 1.5*IQR,
-    piso = Q1 - 1.5*IQR
-  ) %>% 
-  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
-  select(country, income, pbi_p_c, deuda_pbi) %>% 
-  arrange(desc(deuda_pbi))
-
-print("Para el año 2023, estos paises fueron outliers dentro de sus grupos: ")
-print(paises_outliers_2023)
-
-# Grafico del boxplot 
-
-boxplot_2023 <- ggplot(anio_2023, aes(x = income, y = deuda_pbi, fill = income)) +
-  geom_boxplot(alpha = 0.8) + 
-  
-  # Escala de colores manuales
-  
-  scale_fill_manual(values = c(
-    "High income"         = "#2E8B57",  
-    "Upper middle income" = "#9ACD32",  
-    "Lower middle income" = "#FFA500",  
-    "Low income"          = "#CD5C5C"   
-  )) +
-  
-  # Titulos y etiquetas
-  
-  labs(
-    title = "Distribución de la Deuda Pública para el año 2023",
-    subtitle = "Ordenado segun ingresos de los paises",
-    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
-    x = "",
-    y = "Deuda como % del PBI"
+graf_conteo <- ggplot(datos_conteo, aes(x = factor(year), y = cantidad_paises, fill = income)) +
+  geom_col(position = "stack", width = 0.6) +
+  geom_text(aes(label = cantidad_paises), 
+            position = position_stack(vjust = 0.5),
+            color = "black", size = 5) +
+  geom_label(data = totales_por_anio,
+             aes(x = factor(year), y = total, label = total),
+             inherit.aes = FALSE,
+             fill = "white",     # Color de fondo del cuadrito
+             color = "black",    # Color del número
+             size = 5, 
+             fontface = "bold",
+             vjust = -0.25,          # 0 = Apoyado justo sobre la línea final de la barra
+             label.size = 0.5    # Grosor del borde del cuadrito (0 = sin borde)
   ) +
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",
+    "Upper middle income" = "#9ACD32",
+    "Lower middle income" = "#FFA500",
+    "Low income"          = "#CD5C5C"
+  )) +
+  labs(title = "Composición de la Muestra por Nivel de Ingreso",
+       subtitle = "Cantidad de países en cada categoría por año",
+       caption = "Elaboración propia segun Banco Mundial y Datos Macro",
+       x = "",
+       y = "Cantidad de Países",
+       fill = "Ingreso") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        plot.caption = element_text(hjust = 0))
   
-  # Temas del grafico
-  
+# GRAFICO de MEDIANA y MEDIA
+
+graf_mediana <- ggplot(analisis_estadistico, aes(x = income, y = mediana, fill = income)) +
+  geom_col(alpha = 0.8, color = "black") +
+  facet_wrap(~year) +
+  geom_text(aes(label = round(mediana, 1)), 
+            vjust = -0.5, size = 3.5, fontface = "bold") +
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",
+    "Upper middle income" = "#9ACD32",
+    "Lower middle income" = "#FFA500",
+    "Low income"          = "#CD5C5C"
+  )) +
+  labs(title = "Evolución de la Deuda Mediana",
+       subtitle = "Comparación por nivel de ingreso",
+       caption = "Elaboración propia segun Banco Mundial y Datos Macro",
+       y = "Deuda Promedio (% PBI)",
+       x = "",        
+       fill = "Nivel de Ingreso") + 
   theme_minimal() +
   theme(
-    legend.position = "none",
-    axis.text.x = element_text(size = 10, face = "bold"),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(), 
+    legend.position = "bottom",
+    strip.text = element_text(size = 12, face = "bold"), 
+    panel.grid.major.x = element_blank(),
     plot.caption = element_text(hjust = 0)
-  ) +
-  
-  # Etiqueta outliers
-  
-  geom_text_repel(data = paises_outliers_2023,   
-                  aes(label = country),     
-                  size = 4,                 
-                  box.padding = 0.4
   )
 
-print(boxplot_2023)
-
-#======================================================================
-#                   BOXPLOT CONJUNTO                                  #                   
-#======================================================================
-
-anio_todos <- tabla_imputar %>% 
-  mutate(income = factor(income, levels = c("High income", 
-                                            "Upper middle income", 
-                                            "Lower middle income", 
-                                            "Low income")))
-
-paises_outliers_todos <- anio_todos %>%
-  group_by(year, income) %>% 
-  mutate(
-    Q1 = quantile(deuda_pbi, 0.25),
-    Q3 = quantile(deuda_pbi, 0.75),
-    IQR = IQR(deuda_pbi),
-    techo = Q3 + 1.5 * IQR,
-    piso  = Q1 - 1.5 * IQR
-  ) %>%
-  ungroup() %>%
-  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
-  select(country, year, income, pbi_p_c, deuda_pbi) %>% 
-  arrange(desc(deuda_pbi))
-
-print("Todos los outliers")
-print(paises_outliers_todos)
-
-boxplot_evolucion <- ggplot(tabla_imputar, aes(x = income, y = deuda_pbi, fill = income)) +
-  geom_boxplot(alpha = 0.8) + 
-  facet_wrap(~ year) + # Dividido en tres paneles
+graf_media <- ggplot(analisis_estadistico, aes(x = income, y = media, fill = income)) +
+  geom_col(alpha = 0.8, color = "black") +
+  facet_wrap(~year) +
+  geom_text(aes(label = round(media, 1)), 
+            vjust = -0.5, size = 3.5, fontface = "bold") +
   scale_fill_manual(values = c(
-    "High income"         = "#2E8B57",  
-    "Upper middle income" = "#9ACD32",  
-    "Lower middle income" = "#FFA500",  
-    "Low income"          = "#CD5C5C"   
+    "High income"         = "#2E8B57",
+    "Upper middle income" = "#9ACD32",
+    "Lower middle income" = "#FFA500",
+    "Low income"          = "#CD5C5C"
   )) +
+  labs(title = "Evolución de la Deuda Promedio (Media)",
+       subtitle = "Comparación por nivel de ingreso",
+       caption = "Elaboración propia segun Banco Mundial y Datos Macro",
+       y = "Deuda Promedio (% PBI)",
+       x = "",        
+       fill = "Nivel de Ingreso") + 
+  theme_minimal() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(), 
+    legend.position = "bottom",
+    strip.text = element_text(size = 12, face = "bold"), 
+    panel.grid.major.x = element_blank(),
+    plot.caption = element_text(hjust = 0)
+  )
+
+# GRAFICO de HISTOGRAMA
+
+graf_histograma <- ggplot(tabla_imputar, aes(x = deuda_pbi)) +
+  # Histograma
+  geom_histogram(aes(y = after_stat(density)), 
+                 bins = 25, 
+                 fill = "#5D6D7E", 
+                 color = "white", 
+                 alpha = 0.6) +
+  
+  # Distribución
+  geom_density(color = "#1A5276",  
+               fill = "#AED6F1",   
+               alpha = 0.3,        
+               size = 1) +
+  
+  # Lineas de media y mediana
+  geom_vline(data = metricas_por_anio, 
+             aes(xintercept = media), 
+             color = "#2E86C1",  
+             linetype = "dashed", 
+             size = 1) +
+  
+  geom_vline(data = metricas_por_anio, 
+             aes(xintercept = mediana), 
+             color = "#E74C3C",  
+             linetype = "solid", 
+             size = 1) +
+  
+  facet_wrap(~year , ncol = 1) +
+  
   labs(
-    title = "Evolución de la Deuda Pública (2017 - 2023)",
-    subtitle = "Comparación de distribución y outliers por año y nivel de ingreso",
-    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
-    x = "",
-    y = "Deuda como % del PBI"
+    title = "Distribucion de la Deuda Pública",
+    subtitle = "Rojo (Sólida) = Mediana | Azul (Punteada) = Media",
+    caption = "Elaboración propia segun Banco Mundial y Datos Macro",
+    x = "Deuda Pública (% PBI)",
+    y = "Cantidad de Países"
   ) +
   theme_minimal() +
   theme(
-    legend.position = "none",
-    axis.text.x = element_text(size = 8, face = "bold", angle = 45, hjust = 1), 
-    plot.caption = element_text(hjust = 0),
-    strip.text = element_text(size = 12, face = "bold") 
-  ) +
-  geom_text_repel(data = paises_outliers_todos,   
-                  aes(label = country),      
-                  size = 3,                  
-                  box.padding = 0.4,
-                  max.overlaps = 15 # Un poco más de tolerancia para que muestre varios
-  )
+    plot.caption = element_text(hjust = 0))
 
-print(boxplot_evolucion)
+print(graf_histograma)
+
+
