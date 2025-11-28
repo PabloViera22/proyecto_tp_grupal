11e8f6c50ad11ac897c34fd892071bf265881f09
geroosilva <silvageronimofelipe2002@gmail.com>
2025-11-28 02:56:23 -0300

    cambio de nombre del script

---
+++ scripts/06_deteccion_outliers.R
@@ -0,0 +1,359 @@
+# Agregar funciones o parametros, no se bien como se hace. Eliminar cuando ya este
+
+library(ggrepel) # Agregar esta libreria a los nuevos parametros
+
+# Importacion de scipt anterior
+
+# IMPORTANTE!!
+# Agregar tabla con el nuevo link cuando el repositorio sea publico (borrar comentario cuando se haga)
+tabla_imputar <- read.csv("https://raw.githubusercontent.com/PabloViera22/proyecto_tp_grupal/refs/heads/main/data/processed/tabla_para_imputacion.csv?token=GHSAT0AAAAAADPYWERTDILAZ5KNJANN25G42JI5MCQ") 
+
+#======================================================================
+#                   AÑO 2017: El mundo pre-pandemia                    #                   
+#======================================================================
+
+anio_2017 <- tabla_imputar %>% 
+  filter(year == 2017) %>% 
+  mutate(income = factor(income, levels = c("High income", 
+                                          "Upper middle income", 
+                                          "Lower middle income", 
+                                          "Low income")))
+
+#==========================================
+# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
+#==========================================
+
+# Ordeno segun ingreso
+
+# Analizaremos segun ingreso de cada pais
+
+anio_2017 %>%
+  group_by(income) %>%
+  summarise(
+    cantida_paises = n_distinct(country),
+    mediana = median(deuda_pbi),
+    media = mean(deuda_pbi),
+    desvio = sd(deuda_pbi),
+    iqr = IQR(deuda_pbi),
+    minimo = min(deuda_pbi),
+    maximo = max(deuda_pbi)
+  ) %>% 
+  arrange(income)
+
+#==================================
+# ANALISIS DEL BOXPLOT y OUTLIERS #
+#==================================
+
+# Outliers dentro de cada grupo
+
+paises_outliers_2017 <- anio_2017 %>% 
+  group_by(income) %>% 
+  mutate(
+    Q1 = quantile(deuda_pbi, 0.25),
+    Q3 = quantile(deuda_pbi, 0.75),
+    IQR = IQR(deuda_pbi),
+    techo = Q3 + 1.5*IQR,
+    piso = Q1 - 1.5*IQR
+  ) %>% 
+  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
+  select(country, income, pbi_p_c, deuda_pbi) %>% 
+  arrange(desc(deuda_pbi))
+  
+print("Para el año 2017, estos paises fueron outliers dentro de sus grupos: ")
+print(paises_outliers_2017)
+
+# Grafico del boxplot 
+
+boxplot_2017 <- ggplot(anio_2017, aes(x = income, y = deuda_pbi, fill = income)) +
+  geom_boxplot(alpha = 0.8) + 
+  
+  # Escala de colores manuales
+    
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",  
+    "Upper middle income" = "#9ACD32",  
+    "Lower middle income" = "#FFA500",  
+    "Low income"          = "#CD5C5C"   
+  )) +
+  
+  # Titulos y etiquetas
+  
+  labs(
+    title = "Distribución de la Deuda Pública para el año 2017",
+    subtitle = "Ordenado segun ingresos de los paises",
+    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
+    x = "",
+    y = "Deuda como % del PBI"
+  ) +
+  
+  # Temas del grafico
+  
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(size = 10, face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) +
+      
+  # Etiqueta outliers
+  
+  geom_text_repel(data = paises_outliers_2017,   
+                  aes(label = country),     
+                  size = 4,                 
+                  box.padding = 0.4
+                  )
+
+print(boxplot_2017)
+
+#======================================================================
+#                   AÑO 2020: El mundo en pandemia                    #                   
+#======================================================================
+  
+anio_2020 <- tabla_imputar %>% 
+  filter(year == 2020) %>% 
+  mutate(income = factor(income, levels = c("High income", 
+                                            "Upper middle income", 
+                                            "Lower middle income", 
+                                            "Low income")))
+
+#==========================================
+# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
+#==========================================
+
+# Ordeno segun ingreso
+
+# Analizaremos segun ingreso de cada pais
+
+anio_2020 %>%
+  group_by(income) %>%
+  summarise(
+    cantida_paises = n_distinct(country),
+    mediana = median(deuda_pbi),
+    media = mean(deuda_pbi),
+    desvio = sd(deuda_pbi),
+    iqr = IQR(deuda_pbi),
+    minimo = min(deuda_pbi),
+    maximo = max(deuda_pbi)
+  ) %>% 
+  arrange(income)
+
+#==================================
+# ANALISIS DEL BOXPLOT y OUTLIERS #
+#==================================
+
+# Outliers dentro de cada grupo
+
+paises_outliers_2020 <- anio_2020 %>% 
+  group_by(income) %>% 
+  mutate(
+    Q1 = quantile(deuda_pbi, 0.25),
+    Q3 = quantile(deuda_pbi, 0.75),
+    IQR = IQR(deuda_pbi),
+    techo = Q3 + 1.5*IQR,
+    piso = Q1 - 1.5*IQR
+  ) %>% 
+  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
+  select(country, income, pbi_p_c, deuda_pbi) %>% 
+  arrange(desc(deuda_pbi))
+
+print("Para el año 2020, estos paises fueron outliers dentro de sus grupos: ")
+print(paises_outliers_2020)
+
+# Grafico del boxplot 
+
+boxplot_2020 <- ggplot(anio_2020, aes(x = income, y = deuda_pbi, fill = income)) +
+  geom_boxplot(alpha = 0.8) + 
+  
+  # Escala de colores manuales
+  
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",  
+    "Upper middle income" = "#9ACD32",  
+    "Lower middle income" = "#FFA500",  
+    "Low income"          = "#CD5C5C"   
+  )) +
+  
+  # Titulos y etiquetas
+  
+  labs(
+    title = "Distribución de la Deuda Pública para el año 2020",
+    subtitle = "Ordenado segun ingresos de los paises",
+    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
+    x = "",
+    y = "Deuda como % del PBI"
+  ) +
+  
+  # Temas del grafico
+  
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(size = 10, face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) +
+  
+  # Etiqueta outliers
+  
+  geom_text_repel(data = paises_outliers_2020,   
+                  aes(label = country),     
+                  size = 4,                 
+                  box.padding = 0.4
+  )
+
+print(boxplot_2020)
+
+#======================================================================
+#                   AÑO 2023: El mundo post-pandemia                  #                   
+#======================================================================
+
+anio_2023 <- tabla_imputar %>% 
+  filter(year == 2023) %>% 
+  mutate(income = factor(income, levels = c("High income", 
+                                            "Upper middle income", 
+                                            "Lower middle income", 
+                                            "Low income")))
+
+#==========================================
+# MEDIA, MEDIANA y ANALISIS DE DISPERSION # 
+#==========================================
+
+# Ordeno segun ingreso
+
+# Analizaremos segun ingreso de cada pais
+
+anio_2023 %>%
+  group_by(income) %>%
+  summarise(
+    cantida_paises = n_distinct(country),
+    mediana = median(deuda_pbi),
+    media = mean(deuda_pbi),
+    desvio = sd(deuda_pbi),
+    iqr = IQR(deuda_pbi),
+    minimo = min(deuda_pbi),
+    maximo = max(deuda_pbi)
+  ) %>% 
+  arrange(income)
+
+#==================================
+# ANALISIS DEL BOXPLOT y OUTLIERS #
+#==================================
+
+# Outliers dentro de cada grupo
+
+paises_outliers_2023 <- anio_2023 %>% 
+  group_by(income) %>% 
+  mutate(
+    Q1 = quantile(deuda_pbi, 0.25),
+    Q3 = quantile(deuda_pbi, 0.75),
+    IQR = IQR(deuda_pbi),
+    techo = Q3 + 1.5*IQR,
+    piso = Q1 - 1.5*IQR
+  ) %>% 
+  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
+  select(country, income, pbi_p_c, deuda_pbi) %>% 
+  arrange(desc(deuda_pbi))
+
+print("Para el año 2023, estos paises fueron outliers dentro de sus grupos: ")
+print(paises_outliers_2023)
+
+# Grafico del boxplot 
+
+boxplot_2023 <- ggplot(anio_2023, aes(x = income, y = deuda_pbi, fill = income)) +
+  geom_boxplot(alpha = 0.8) + 
+  
+  # Escala de colores manuales
+  
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",  
+    "Upper middle income" = "#9ACD32",  
+    "Lower middle income" = "#FFA500",  
+    "Low income"          = "#CD5C5C"   
+  )) +
+  
+  # Titulos y etiquetas
+  
+  labs(
+    title = "Distribución de la Deuda Pública para el año 2023",
+    subtitle = "Ordenado segun ingresos de los paises",
+    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
+    x = "",
+    y = "Deuda como % del PBI"
+  ) +
+  
+  # Temas del grafico
+  
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(size = 10, face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) +
+  
+  # Etiqueta outliers
+  
+  geom_text_repel(data = paises_outliers_2023,   
+                  aes(label = country),     
+                  size = 4,                 
+                  box.padding = 0.4
+  )
+
+print(boxplot_2023)
+
+#======================================================================
+#                   BOXPLOT CONJUNTO                                  #                   
+#======================================================================
+
+anio_todos <- tabla_imputar %>% 
+  mutate(income = factor(income, levels = c("High income", 
+                                            "Upper middle income", 
+                                            "Lower middle income", 
+                                            "Low income")))
+
+paises_outliers_todos <- anio_todos %>%
+  group_by(year, income) %>% 
+  mutate(
+    Q1 = quantile(deuda_pbi, 0.25),
+    Q3 = quantile(deuda_pbi, 0.75),
+    IQR = IQR(deuda_pbi),
+    techo = Q3 + 1.5 * IQR,
+    piso  = Q1 - 1.5 * IQR
+  ) %>%
+  ungroup() %>%
+  filter(deuda_pbi > techo | deuda_pbi < piso) %>% 
+  select(country, year, income, pbi_p_c, deuda_pbi) %>% 
+  arrange(desc(deuda_pbi))
+
+print("Todos los outliers")
+print(paises_outliers_todos)
+
+boxplot_evolucion <- ggplot(tabla_imputar, aes(x = income, y = deuda_pbi, fill = income)) +
+  geom_boxplot(alpha = 0.8) + 
+  facet_wrap(~ year) + # Dividido en tres paneles
+  scale_fill_manual(values = c(
+    "High income"         = "#2E8B57",  
+    "Upper middle income" = "#9ACD32",  
+    "Lower middle income" = "#FFA500",  
+    "Low income"          = "#CD5C5C"   
+  )) +
+  labs(
+    title = "Evolución de la Deuda Pública (2017 - 2023)",
+    subtitle = "Comparación de distribución y outliers por año y nivel de ingreso",
+    caption = "Elaboracion propia segun Banco Mundial y Datos Macro",
+    x = "",
+    y = "Deuda como % del PBI"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text.x = element_text(size = 8, face = "bold", angle = 45, hjust = 1), 
+    plot.caption = element_text(hjust = 0),
+    strip.text = element_text(size = 12, face = "bold") 
+  ) +
+  geom_text_repel(data = paises_outliers_todos,   
+                  aes(label = country),      
+                  size = 3,                  
+                  box.padding = 0.4,
+                  max.overlaps = 15 # Un poco más de tolerancia para que muestre varios
+  )
+
+print(boxplot_evolucion)
