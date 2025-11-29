327f189b32a94fc75d88081c36e3b26ddd536103
PabloViera22 <p4blo.viera@gmail.com>
2025-11-29 12:15:50 -0300

    cambio de nombre/orden

--- funciones/funciones_regresion.R
+++ funciones/funciones_regresion.R
@@ -11,9 +11,10 @@ prueba_hipotesis<-function(modelo_de_regresion){
   if (-t_critico_comun-deuda_t_obs<0) {print("TEST 1 NORMAL: RECHAZO H_0, deuda tiene signo positivo")}else {print("TEST 1 NORMAL: NO RECHAZO H_0, deuda no tiene signo positivo")}
   if (-t_critico_estricto-deuda_t_obs<0) {print("TEST 1 ESTRICTO: RECHAZO H_0, deuda tiene signo positivo")}else{print("TEST 1 ESTRICTO: NO RECHAZO H_0, deuda no tiene signo positivo")}
   if (-t_critico_laxo-deuda_t_obs<0) {print("TEST 1 LAXO: RECHAZO H_0, deuda tiene signo positivo")}else{print("TEST 1 LAXO: NO RECHAZO H_0, deuda no tiene signo positivo")}
-  if (-t_critico_comun-deuda_cuadrada_t_obs<0) {print("TEST 2 NORMAL: RECHAZO H_0, deuda^2 tiene signo negativo")}else {print("TEST 2 NORMAL: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
-  if (-t_critico_estricto-deuda_cuadrada_t_obs<0) {print("TEST 2 ESTRICTO: RECHAZO H_0, deuda^2 tiene signo negativo")}else{print("TEST 2 ESTRICTO: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
-  if (-t_critico_laxo-deuda_cuadrada_t_obs<0) {print("TEST 2 LAXO: RECHAZO H_0, deuda^2 tiene signo negativo")}else{print("TEST 2 LAXO: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
+
+  if (t_critico_comun-deuda_cuadrada_t_obs>0) {print("TEST 2 NORMAL: RECHAZO H_0, deuda^2 tiene signo negativo")}else {print("TEST 2 NORMAL: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
+  if (t_critico_estricto-deuda_cuadrada_t_obs>0) {print("TEST 2 ESTRICTO: RECHAZO H_0, deuda^2 tiene signo negativo")}else{print("TEST 2 ESTRICTO: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
+  if (t_critico_laxo-deuda_cuadrada_t_obs>0) {print("TEST 2 LAXO: RECHAZO H_0, deuda^2 tiene signo negativo")}else{print("TEST 2 LAXO: NO RECHAZO H_0, deuda^2 no tiene signo negativo")}
 }
 
 
