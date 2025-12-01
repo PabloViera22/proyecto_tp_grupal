bc37faf5a55b25d6844b18b18fd1b44916e3805a
PabloViera22 <p4blo.viera@gmail.com>
2025-12-01 01:09:50 -0300

    cambio nombre, arreglo scrip de imputacion

--- scripts/04_imputacion.R
+++ scripts/04_imputacion.R
@@ -1,5 +1,6 @@
 source(here::here("config", "parametros.R"))
 source(here::here("funciones", "funciones_para_importar_exportar.R"))
+source(here::here("funciones", "funciones_para_imputar.R"))
 # Importamos los datos del anterior script
 tabla_imputar<-cargar_datos(nombre_archivo = "tabla_para_imputacion.csv",carpeta = "processed")
 
@@ -7,6 +8,6 @@ columnas_todas<-names(tabla_imputar)
 col_a_imputar<- c("formacion_bruta_capital", "apertura", "pbi_p_c", "crecimiento_pbi", "consumo_gobierno", "inflacion")
 tabla_imputada<-imputacion_multiple(datos = tabla_imputar, vector_columna = columnas_todas)
 nrow(tabla_imputada)
-
+tabla_imputada
 
 exportar_data(data = tabla_imputada, nombre = "tabla_limpia", carpeta = "clean")
