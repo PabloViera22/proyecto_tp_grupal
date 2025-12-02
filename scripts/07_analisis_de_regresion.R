aff9914e2b7227073b6e0f1072785ec5214eab30
geroosilva <silvageronimofelipe2002@gmail.com>
2025-12-01 23:40:22 -0300

    Se agrega un grafico de la hipotesis

--- scripts/07_analisis_de_regresion.R
+++ scripts/07_analisis_de_regresion.R
@@ -1,6 +1,8 @@
 source(here::here("config", "parametros.R"))
 source(here::here("funciones", "funciones_para_importar_exportar.R"))
 source(here::here("funciones", "funciones_regresion.R"))
+source(here::here("funciones", "funciones_visualizacion.R"))
+
 # IMPORTAMOS DATOS
 tabla_regresion<-cargar_datos(nombre_archivo = "tabla_limpia.csv", carpeta = "clean")
 # HIPOTESIS: DEUDA TIENE REALCION EN FORMA DE U INVERTIDA. EL COEFICIENTE DE DEUDA>0 Y EL COEF. DEUDA^2<0.
@@ -10,6 +12,50 @@ tabla_regresion<-cargar_datos(nombre_archivo = "tabla_limpia.csv", carpeta = "cl
 #    H_1: Beta_1<0
 # 2) H_0: Beta_2<=0
 #    H_1: Beta_2>0
+
+#==============================================================================#
+# VISUALIZACION DE LA HIPOTESIS
+#==============================================================================#
+
+tabla_regresion <- tabla_regresion %>% 
+  mutate(income = factor(income, levels = c("High income", 
+                                            "Upper middle income", 
+                                              "Lower middle income", 
+                                              "Low income")))
+
+
+graf_hipotesis <- ggplot(tabla_regresion, aes(x = deuda_pbi, y = crecimiento_pbi)) +
+  geom_point(aes(color = income), alpha = 0.6) +
+  geom_smooth(method = "lm", formula = y ~ x + I(x^2), 
+              color = "black", size = 1, se = FALSE) +
+  facet_wrap(~year, scales = "free_y") +
+  labs(title = "Relación Deuda vs. Crecimiento",
+       subtitle = "Test visual de la hipótesis de U Invertida (Línea negra = Ajuste cuadrático)",
+       x = "Deuda Pública (% PBI)",
+       y = "Crecimiento del PBI (%)",
+       color = "Ingreso") +
+  scale_color_manual(values = c(
+    "High income"         = "#2E8B57",
+    "Upper middle income" = "#9ACD32",
+    "Lower middle income" = "#FFA500",
+    "Low income"          = "#CD5C5C"
+  )) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 14, face = "bold"),
+    plot.subtitle = element_text(size = 12, color = "gray60"),
+    axis.title = element_text(size = 11),
+    axis.text = element_text(size = 10),
+    legend.title = element_text(size = 11),
+    legend.text = element_text(size = 10),
+    strip.text = element_text(size = 10, face = "bold"),
+    panel.grid.minor = element_blank(),
+    legend.position = "bottom",
+    axis.text.x = element_text(size = 10, face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) 
+  
+guardar_grafico(graf_hipotesis, "graf_hipotesis")
 
 #==============================================================================#
 # MODELO REGRESION GENERAL CORROBARCION DE HIPÓTESIS
